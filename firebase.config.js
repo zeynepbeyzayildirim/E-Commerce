a927f8ae005d29ffc24388b3b94064e8effc137f
Zeynep Beyza <zynpbyza0@gmail.com>
2022-11-22 13:42:01 +0300

    .env file
    Firebase connected api key

---
+++ firebase.config.js
@@ -0,0 +1,18 @@
+import { getApp,getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBLgKGgotH_L-6zgvnzwg0dMG02Oebaesk",
+    authDomain: "ecommerce-aa340.firebaseapp.com",
+    databaseURL: "https://ecommerce-aa340-default-rtdb.firebaseio.com",
+    projectId: "ecommerce-aa340",
+    storageBucket: "ecommerce-aa340.appspot.com",
+    messagingSenderId: "378170397151",
+    appId: "1:378170397151:web:021b0f8f40086df5ea746a"
+  };
+
+  const app = getApps.Length > 0 ? getApp() : initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const storage = getStorage(app);
